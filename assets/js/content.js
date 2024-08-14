490cb4e9be0b904b3afde5c43c0696c907a2ecfd
--replace-all <surya.indian321@gmail.com>
2024-08-14 10:41:09 +0530

    Arabic Translation Added

---
+++ assets/js/content.js
@@ -0,0 +1,218 @@
+const translation = {
+  en: {
+    navbar: [
+      'Home',
+      'About Us',
+      'Recruitment Process',
+      'Services',
+      'Job Seekers',
+      'Clients',
+      'Contact Us',
+    ],
+    about: {
+      heading: 'About Us',
+      desc: 'We offer personalized recruitment and consultancy services to meet your unique needs.',
+      subheading: ['Who We Are', 'Our Vision', 'Our Mission', 'Our Value'],
+      subdesc: [
+        'At Premium Travel Consultant, we specialize in bridging the gap between top-tier talent and exceptional employers. Founded in 2010, our mission is to empower individuals and organizations by providing expert recruitment and consultancy services tailored to their unique needs.',
+        'To be the leading consultancy and recruitment agency known for our integrity, innovation, and impact in transforming careers and businesses.',
+        'To connect the right people with the right opportunities, fostering growth and success for both job seekers and employers.',
+      ],
+      listheading: ['Integrity', 'Excellence', 'Partnership', 'Innovation'],
+      listdesc: [
+        'We uphold the highest standards of honesty and ethics in all our interactions.',
+        'We strive for excellence in everything we do, delivering superior services and results.',
+        'We build lasting relationships based on trust and collaboration.',
+        'We embrace innovative solutions to meet the evolving needs of our clients and candidates.',
+      ],
+    },
+    counts: [
+      'Completed Project',
+      'Happy Customers',
+      'Manpower Recruited',
+      'Country Presence',
+    ],
+    clients: {
+      heading: 'Our Clients',
+      desc: 'We have worked with a diverse range of clients across various industries.',
+    },
+    faqs: {
+      heading: 'FAQs',
+      desc: 'Find your answer and if any doubt contact me anytime',
+      questions: [
+        'How can I get in touch with Premium Travel Consultant?',
+        'Do you offer remote services?',
+        'How can I staty pudated with Premium Travel Consultant latest news and job openings?',
+      ],
+      ansers: [
+        'You can contact us via phone at 0522 - 4626555 | 4654555, email us at ptc4lucknow@gmail.com, or visit our office at 789 Career Avenue, Suite 101, Lucknow City.',
+        'Yes, we offer remote services for both job seekers and employers. Our team can provide consultations and support virtually to accommodate your needs.',
+        'You can subscribe to our newsletter and follow us on LinkedIn, Facebook, and Twitter to stay updated with the latest career tips, job openings, and industry insights.',
+      ],
+    },
+    contact: {
+      heading: 'Contact Us',
+      desc: 'Get in touch with us to learn more about our services and how we can help',
+      labels: [
+        'First Name',
+        'Last Name',
+        'Emails',
+        'Mobile Number',
+        'Message',
+        'Submit',
+      ],
+    },
+  },
+  ar: {
+    navbar: [
+      'الصفحة الرئيسية',
+      'من نحن',
+      'عملية التوظيف',
+      'الخدمات',
+      'الباحثون عن العمل',
+      'العملاء',
+      'اتصل بنا',
+    ],
+    about: {
+      heading: 'من نحن',
+      desc: 'نحن نقدم خدمات التوظيف والاستشارات الشخصية لتلبية احتياجاتك الفريدة.',
+      subheading: ['من نحن', 'رؤيتنا', 'مهمتنا', 'قيمنا'],
+      subdesc: [
+        'في شركة بريميوم للاستشارات السياحية، نتخصص في سد الفجوة بين المواهب ذات المستوى العالي وأصحاب العمل الاستثنائيين. تأسست في عام 2010، مهمتنا هي تمكين الأفراد والمنظمات من خلال تقديم خدمات التوظيف والاستشارات الخبيرة المصممة خصيصًا لتلبية احتياجاتهم الفريدة.',
+        'أن نكون الوكالة الرائدة في مجال الاستشارات والتوظيف المعروفة بنزاهتنا وابتكارنا وتأثيرنا في تحويل الوظائف والأعمال.',
+        'ربط الأشخاص المناسبين بالفرص المناسبة، وتعزيز النمو والنجاح لكل من الباحثين عن العمل وأصحاب العمل.',
+      ],
+      listheading: ['النزاهة', 'التميز', 'الشراكة', 'الابتكار'],
+      listdesc: [
+        'نحن نحافظ على أعلى معايير الصدق والأخلاق في جميع تفاعلاتنا.',
+        'نسعى لتحقيق التميز في كل ما نقوم به، وتقديم خدمات ونتائج متفوقة.',
+        'نبني علاقات دائمة قائمة على الثقة والتعاون.',
+        'نحن نتبنى الحلول المبتكرة لتلبية الاحتياجات المتطورة لعملائنا والمرشحين.',
+      ],
+    },
+    counts: [
+      'المشروع المكتمل',
+      'العملاء السعداء',
+      'القوى العاملة التي تم توظيفها',
+      'الحضور في الدولة',
+    ],
+    clients: {
+      heading: 'عملاؤنا',
+      desc: 'لقد عملنا مع مجموعة متنوعة من العملاء في مختلف الصناعات.',
+    },
+    faqs: {
+      heading: 'الأسئلة الشائعة',
+      desc: 'ابحث عن إجابتك وإذا كان لديك أي شك تواصل معي في أي وقت',
+      questions: [
+        'كيف يمكنني التواصل مع شركة بريميوم للاستشارات السياحية؟',
+        'هل تقدمون خدمات عن بُعد؟',
+        'كيف يمكنني البقاء على اطلاع بأحدث الأخبار وفرص العمل في شركة بريميوم للاستشارات السياحية؟',
+      ],
+      ansers: [
+        'يمكنك الاتصال بنا عبر الهاتف على الرقم 0522 - 4626555 | 4654555، أو مراسلتنا عبر البريد الإلكتروني على ptc4lucknow@gmail.com، أو زيارة مكتبنا في 789 Career Avenue، جناح 101، مدينة لكناو.',
+        'نعم، نحن نقدم خدمات عن بُعد لكل من الباحثين عن العمل وأصحاب العمل. يمكن لفريقنا تقديم الاستشارات والدعم عن بُعد لتلبية احتياجاتك.',
+        'يمكنك الاشتراك في نشرتنا الإخبارية ومتابعتنا على لينكد إن وفيسبوك وتويتر للبقاء على اطلاع بآخر نصائح الحياة المهنية، وفرص العمل، وأفكار الصناعة.',
+      ],
+    },
+    contact: {
+      heading: 'اتصل بنا',
+      desc: 'تواصل معنا لمعرفة المزيد عن خدماتنا وكيف يمكننا مساعدتك',
+      labels: [
+        'الاسم الأول',
+        'الاسم الأخير',
+        'البريد الإلكتروني',
+        'رقم الجوال',
+        'الرسالة',
+        'إرسال',
+      ],
+    },
+  },
+};
+
+// Language Select
+const language = document.getElementById('language');
+// Navbar Selection
+let homeNav = document.getElementById('home-nav');
+let aboutNav = document.getElementById('about-nav');
+let processNav = document.getElementById('process-nav');
+let servicesNav = document.getElementById('services-nav');
+let jobNav = document.getElementById('job-nav');
+let clientNav = document.getElementById('client-nav');
+let contactNav = document.getElementById('contact-nav');
+
+// About
+let aboutHeading = document.getElementById('about-heading');
+let aboutDesc = document.getElementById('about-desc');
+let aboutSubheading1 = document.getElementById('about-subheading1');
+let aboutDesc1 = document.getElementById('about-desc1');
+let aboutSubheading2 = document.getElementById('about-subheading2');
+let aboutDesc2 = document.getElementById('about-desc2');
+let aboutSubheading3 = document.getElementById('about-subheading3');
+let aboutDesc3 = document.getElementById('about-desc3');
+let aboutSubheading4 = document.getElementById('about-subheading4');
+let aboutListHeading1 = document.getElementById('about-list-heading1');
+let aboutListDesc1 = document.getElementById('about-list-desc1');
+let aboutListHeading2 = document.getElementById('about-list-heading2');
+let aboutListDesc2 = document.getElementById('about-list-desc2');
+let aboutListHeading3 = document.getElementById('about-list-heading3');
+let aboutListDesc3 = document.getElementById('about-list-desc3');
+let aboutListHeading4 = document.getElementById('about-list-heading4');
+let aboutListDesc4 = document.getElementById('about-list-desc4');
+
+language.addEventListener('change', (e) => {
+  setLanguage(e.target.value);
+  // console.log(e.target.value);
+});
+
+const setLanguage = (lang) => {
+  if (lang == 'ar') {
+    // Navbar in Arabic
+    homeNav.innerText = translation.ar.navbar[0];
+    aboutNav.innerText = translation.ar.navbar[1];
+    processNav.innerText = translation.ar.navbar[2];
+    servicesNav.innerText = translation.ar.navbar[3];
+    jobNav.innerText = translation.ar.navbar[4];
+    clientNav.innerText = translation.ar.navbar[5];
+    contactNav.innerText = translation.ar.navbar[6];
+
+    // About
+    aboutHeading.innerText = translation.ar.about.heading;
+    aboutDesc.innerText = translation.ar.about.desc;
+    aboutSubheading1.innerText = translation.ar.about.subheading[0];
+    aboutDesc1.innerText = translation.ar.about.subdesc[0];
+    aboutSubheading2.innerText = translation.ar.about.subheading[1];
+    aboutDesc2.innerText = translation.ar.about.subdesc[1];
+    aboutSubheading3.innerText = translation.ar.about.subheading[2];
+    aboutDesc3.innerText = translation.ar.about.subdesc[2];
+    aboutSubheading4.innerText = translation.ar.about.subheading[3];
+    aboutListHeading1.innerText = translation.ar.about.listheading[0];
+    aboutListDesc1.innerText = translation.ar.about.listdesc[0];
+    aboutListHeading2.innerText = translation.ar.about.listheading[1];
+    aboutListDesc2.innerText = translation.ar.about.listdesc[1];
+    aboutListHeading3.innerText = translation.ar.about.listheading[2];
+    aboutListDesc3.innerText = translation.ar.about.listdesc[2];
+    aboutListHeading4.innerText = translation.ar.about.listheading[3];
+    aboutListDesc4.innerText = translation.ar.about.listdesc[3];
+    console.log(translation.ar.about.listdesc[3]);
+  } else if (lang == 'en') {
+    // Navbar in English
+    homeNav.innerText = translation.en.navbar[0];
+    aboutNav.innerText = translation.en.navbar[1];
+    processNav.innerText = translation.en.navbar[2];
+    servicesNav.innerText = translation.en.navbar[3];
+    jobNav.innerText = translation.en.navbar[4];
+    clientNav.innerText = translation.en.navbar[5];
+    contactNav.innerText = translation.en.navbar[6];
+
+    // About
+    aboutHeading.innerText = translation.en.about.heading;
+    aboutDesc.innerText = translation.en.about.desc;
+    aboutSubheading1.innerText = translation.en.about.subheading[0];
+    aboutDesc1.innerText = translation.en.about.subdesc[0];
+    aboutSubheading2.innerText = translation.en.about.subheading[1];
+    aboutDesc2.innerText = translation.en.about.subdesc[1];
+    aboutSubheading3.innerText = translation.en.about.subheading[2];
+    aboutDesc3.innerText = translation.en.about.subdesc[2];
+    aboutSubheading4.innerText = translation.en.about.subheading[3];
+  }
+};
